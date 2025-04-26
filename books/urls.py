e9227a13ef4212842605841b9b96884268861af8
Ormir <150350149+Xemmyr@users.noreply.github.com>
2025-04-26 18:38:53 +0200

    Added working add, edit and delete functions

--- books/urls.py
+++ books/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('books/', views.book_list, name='book_list'),
     path('scrape/', views.scrape_books_view, name='scrape_books'),
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/edit/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'),
+
 
 ]
--- books/views.py
+++ books/views.py
@@ -4,8 +4,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Book
+from .forms import BookForm
 
-
+@login_required
 def book_list(request):
     if request.user.is_superuser:
         books = Book.objects.all()
@@ -13,9 +14,47 @@ def book_list(request):
         books = Book.objects.filter(owner=request.user)
     return render(request, 'books/book_list.html', {'books': books})
 
+@login_required
 def scrape_books_view(request):
     scrape_books(request.user)
     return redirect('book_list')
+
+@login_required
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            book = form.save(commit=False)
+            book.owner = request.user
+            book.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'books/add_book.html', {'form': form})
+
+@login_required
+def edit_book(request, book_id):
+    book = Book.objects.get(id=book_id)
+    if request.user != book.owner and not request.user.is_superuser:
+        return redirect('book_list')
+
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'books/edit_book.html', {'form': form})
+
+@login_required
+def delete_book(request, book_id):
+    book = Book.objects.get(id=book_id)
+    if request.user == book.owner or request.user.is_superuser:
+        book.delete()
+
+    return redirect('book_list')
+
 
 def register_view(request):
     if request.method == 'POST':
